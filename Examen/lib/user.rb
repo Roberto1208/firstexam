d5c720429d24ea275a62df857fb2dedfb71335ac
Roberto1208 <robdan_1208@hotmail.com>
2014-06-05 08:51:50 -0700

    first commit

--- Examen/lib/user.rb
+++ Examen/lib/user.rb
@@ -1,5 +1,24 @@
+require 'yaml'
+
+attr_accesor :Posts
 class User
 
+  def add_post(title, txet, date, user)
+   post = Post.new(title, text, date, user)
+     .posts << post
+  end
 
+  def post
+    
+  end
+
+  def list 
+     @post.displa_yentry.each do |post|
+      puts post
+  end
+
+  def find_post_by_title (title)
+
+  end
 
 end
