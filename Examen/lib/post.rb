6592f0f0128970412f417bb5ca7c9f98911e8828
Roberto1208 <robdan_1208@hotmail.com>
2014-06-05 07:39:43 -0700

    first commit

---
+++ Examen/lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+
+
+def summary 
+b = Array.new
+    i = 0
+    user.text.each_line(" ") do |a|
+    if i < 10
+    b.push a
+    i += 1
+    end
+end
+
+def tagme
+end
+
+def same
+end
+
+
+end
